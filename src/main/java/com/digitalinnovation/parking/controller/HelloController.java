307f7d4c2da2b8781216e5cab19e93ce41c8307a
Augusto <augustopelzer@gmail.com>
2022-08-20 16:43:30 -0300

    Estrutura inicial

---
+++ src/main/java/com/digitalinnovation/parking/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.digitalinnovation.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/") //não necessário quando é / mas interessante para deixar explícito
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello DIO! Hohoho Hihihi You";
+    }
+
+}
